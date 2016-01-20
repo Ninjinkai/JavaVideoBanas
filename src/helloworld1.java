3c9e5bdb6267560bb37f5f3b098056c943cfb53f
Ninjinkai <nickpetty@hotmail.com>
2016-01-19 21:28:19 -0500

    Mid video 1.

---
+++ src/helloworld1.java
@@ -0,0 +1,14 @@
+public class helloworld1 {
+	static String randomString = "String to print.";
+	static final double PINUM = 3.1415926535;
+	
+	public static void main(String[] args) {
+		System.out.println(randomString);
+		System.out.println(PINUM);
+		
+		int integerOne = 22;
+		int integerTwo = integerOne + 1;
+		integerTwo--;
+		System.out.println(integerTwo);
+	}
+}
