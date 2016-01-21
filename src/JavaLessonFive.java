940a4a07ecffaded46b9d71b0f1413b501671d37
Ninjinkai <nickpetty@hotmail.com>
2016-01-20 22:20:53 -0500

    Started video 5.

---
+++ src/JavaLessonFive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaLessonFive {
+	
+	static double myPI = 3.1415926;
+	
+	public static int addThem(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(addThem(1, 2));
+
+	}
+
+}
