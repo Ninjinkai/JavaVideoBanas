060b3ded3db6a36cab889f9698d02e88af922281
Ninjinkai <nickpetty@hotmail.com>
2016-01-20 22:32:49 -0500

    Finished lesson five.

--- src/JavaLessonFive.java
+++ src/JavaLessonFive.java
@@ -3,14 +3,39 @@ import java.util.Scanner;
 public class JavaLessonFive {
 	
 	static double myPI = 3.1415926;
+	static int randNum;
+	static Scanner userInput = new Scanner(System.in);
 	
 	public static int addThem(int a, int b) {
 		return a + b;
 	}
+	
+	public static int getRandom() {
+		return randNum = (int) (Math.random() * 51);
+	}
+	
+	public static int checkGuess(int guess) {
+		if (guess == randNum) {
+			return -1;
+		}
+		else {
+			return guess;
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println(addThem(1, 2));
-
+		
+		System.out.println(getRandom());
+		
+		int guessResult = 1;
+		int randomGuess = 0;
+		
+		while (guessResult != -1) {
+			System.out.print("Guess a number: ");
+			randomGuess = userInput.nextInt();
+			guessResult = checkGuess(randomGuess);
+		}
 	}
 
 }
