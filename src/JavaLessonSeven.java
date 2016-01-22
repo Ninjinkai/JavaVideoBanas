302eccdbfe967d93e15ef4c388dac3b6d38c6d62
Ninjinkai <nickpetty@hotmail.com>
2016-01-21 22:32:06 -0500

    Finished video 7.

---
+++ src/JavaLessonSeven.java
@@ -0,0 +1,11 @@
+
+public class JavaLessonSeven {
+
+	public static void main(String[] args) {
+		Monster Frank = new Monster();
+		System.out.println(Frank.name);
+		System.out.println(Frank.getAttack());
+
+	}
+
+}
