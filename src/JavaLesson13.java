6bda54e155dbee398ee00f6a9e4c79d7f1e01b17
Ninjinkai <nickpetty@hotmail.com>
2016-01-24 22:13:28 -0500

    Video 13 done.

--- src/JavaLesson13.java
+++ src/JavaLesson13.java
@@ -3,8 +3,21 @@ import java.util.Arrays;
 public class JavaLesson13 {
 
 	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("Random words and letters.");
+		System.out.println(sb);
 		
+		sb.append(" Moar.");
+		System.out.println(sb);
+		
+		sb.delete(6, 12);
+		System.out.println(sb);
 
+		System.out.println("sb capacity: " + sb.capacity());
+		sb.ensureCapacity(64);
+		System.out.println("sb capacity: " + sb.capacity());
+		sb.trimToSize();
+		System.out.println("sb capacity: " + sb.capacity());
+		
 	}
 
 }
